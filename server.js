6876f5b0d69f3ab217d6bb03cb696ef62132448b
Manuel Joseph <jmpak80@gmail.com>
2011-06-29 15:44:39 +0530

    done with basic http server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
-   res.writeHead(200);
-   res.end('Hello Http\n');
-});
-server.listen(8080);
+http.createServer(function(req, res) {
+    console.log('new request')
+   res.writeHead(200, {
+       'Content-Type' : 'text/plain'
+   });
+   res.end('Hello World\n');
+}).listen(8080);
