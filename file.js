4b5fb065506bd767b337d93122e89525dfd7d7a7
Manuel Joseph <jmpak80@gmail.com>
2011-08-01 13:08:10 +0530

    progress

--- file.js
+++ file.js
@@ -1,27 +1,24 @@
 var http = require('http');
 var fs = require('fs');
-var sys = require('sys');
 
 var port = process.env.PORT || 8080;
 console.log(port);
 
 var file_path = __dirname + "/us.jpg";
 fs.stat(file_path, function(err, stat) {
+    if(err) throw err;
+    var file_size = stat.size;
     
-    http.createServer(function(request, response) {
-        response.writeHead(200, {
-            'Content-Type' : 'image/jpeg',
-            'Content-Length' : stat.size
-        });
-        
-        var rs = fs.createReadStream(file_path);
-        sys.pump(rs, response, function(err) {
-            if (err) {
-                throw err;
-            }
-        });
-
-    }).listen(port); 
+    fs.readFile(file_path, function(err, file_content) {
+        if(err) throw err;
+        http.createServer(function(request, response) {
+            response.writeHead(200, {
+                'Content-Type' : 'image/jpeg',
+                'Content-Length' : stat.size
+            });
+            response.end(file_content);
+        }).listen(port); 
+    });
 }); 
 
 
