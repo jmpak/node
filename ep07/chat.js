af3cc157c16418b6e32eb58880038e352503d279
Manuel Joseph <jmpak80@gmail.com>
2011-08-04 15:49:44 +0530

    deploying to heroku

--- ep07/chat.js
+++ ep07/chat.js
@@ -16,24 +16,24 @@ var server = http.createServer(function(request, response) {
   
 });
 
-// var socket = io.listen(server);
-// 
-// socket.sockets.on('connection', function(client) {
-//   
-//   var username;
-//   
-//   client.send('Welcome to this socket.io chat server!');
-//   client.send('Please input your username: ');
-//   
-//   client.on('message', function(message) {
-//     if (!username) {
-//       username = message;
-//       client.send('Welcome, ' + username + '!');
-//       return;
-//     }
-// w    socket.sockets.send(username + ' sent: ' + message);
-//   });
-//   
-// });
-// 
+var socket = io.listen(server);
+
+socket.sockets.on('connection', function(client) {
+  
+  var username;
+  
+  client.send('Welcome to this socket.io chat server!');
+  client.send('Please input your username: ');
+  
+  client.on('message', function(message) {
+    if (!username) {
+      username = message;
+      client.send('Welcome, ' + username + '!');
+      return;
+    }
+w    socket.sockets.send(username + ' sent: ' + message);
+  });
+  
+});
+
 server.listen(port);
